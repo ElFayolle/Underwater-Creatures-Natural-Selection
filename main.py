1578fee9df57502ffb2fdb6e0bbfbe06f770a2c5
Tom Back <tom.back@etu.minesparis.psl.eu>
2025-06-30 14:07:01 +0200

    modif creature

--- main.py
+++ main.py
@@ -9,6 +9,34 @@ pygame.display.set_caption("Natural Selection Simulation")
 clock = pygame.time.Clock()
 # Main loop
 running = True
+
+forces = []
+accelerations = []
+a = []
+v = []
+dx = []
+
+
+def force_rappel(i,j,creature):
+    k = 0.5
+    mi,mj = creature[i][0], creature[j][0]
+    l = ((mi[0] - mj[0])**2 + (mi[1] - mj[1])**2)**0.5
+    l0 = creature[i][1][j]
+    u_ij = (mi - mj) / l
+    return -k * (l - l0) * u_ij
+
+def pfd(forces):
+    m = 1
+    accelerations = []
+    for i in range(len(forces)):
+        forces_i = [0, 0]
+        for j in range(len(forces[i])):
+            forces_i += forces[i][j]
+        accelerations.append(forces_i / m)
+    return accelerations
+a.append(accelerations)
+
+
 while running:
     # Handle events
     for event in pygame.event.get():
@@ -23,18 +51,25 @@ while running:
 
     # Cap the frame rate at 60 FPS
     clock.tick(1)
-    L = [[(100.,100.), [0.,200.]], [(100.,300.), [200.,0.]]]
+    pos = [[100,100], [100,300]]
+    neigh = [[0,200], [200,0]]
 
-    accelerations = pfd(forces)
+    dt = 1/60
+
+    
+    accelerations_t = pfd(forces)
+    a.append(accelerations) 
+    v.append(dt*a[-1] + v[-1])
 
 
-    for i in L:
-        for index,j in enumerate(i):
-            if j!= 0:
-                pygame.draw.line(screen, (125,50,0), i[0], L[index][0], 10)
-        pygame.draw.circle(screen, (255,0,0), i[0], 20) 
     
 
+
+    for i, point in enumerate(pos):
+        for j, voisin in enumerate(point):
+            if voisin!= 0:
+                pygame.draw.line(screen, (125,50,0), pos[i], pos[j], 10)
+        pygame.draw.circle(screen, (255,0,0), pos[i], 20) 
 
     pygame.display.flip()
 # Quit Pygame
