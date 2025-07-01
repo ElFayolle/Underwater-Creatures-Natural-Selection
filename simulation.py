f7d224169a1fa86fd618853d532055035eb27baa
Basile <basile.dumas@etu.minesparis.psl.eu>
2025-07-01 16:15:15 +0200

    ajout paramètres pour la simulation

--- simulation.py
+++ simulation.py
@@ -8,3 +8,4 @@ with open("creatures.json", "r", encoding="utf-8") as f:
 creatures_sans_index = [liste[1:] for liste in creatures]  # Enlever l'index de chaque créature
 c = np.array(creatures_sans_index, dtype=object)  # Convertir en tableau numpy d'objets
 d = m.calcul_position(c)
+print(d)
