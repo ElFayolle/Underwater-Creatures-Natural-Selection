4587a907205a3c93219f0421aba6bd5298df49f7
Andea-Brogi <andrea.brogi@etu.minesparis.psl.eu>
2025-07-04 14:02:09 +0200

    Filtres + poussés pour mutations

--- params.py
+++ params.py
@@ -1,5 +1,5 @@
 WIDTH, HEIGHT = 800, 600
-DUREE_SIM = 10  # Durée de la simulation en secondes
+DUREE_SIM = 100  # Durée de la simulation en secondes
 CURRENT_CREATURE = 0
 DT = 1/50
 CURRENT_CREATURE=0
